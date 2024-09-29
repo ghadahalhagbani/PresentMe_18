060718d457d60ca39fd0cf7d3cf9dd47320267db
ghadah <Ghadahx2015@gmail.com>
2024-09-29 16:40:43 +0300

    ghada's update

---
+++ PresentMe_18/Ghadah.swift
@@ -0,0 +1,21 @@
+//
+//  Ghadah.swift
+//  PresentMe_18
+//
+//  Created by Ghadah Alhagbani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Ghadah: View {
+    var body: some View {
+        VStack{
+            Image(.IMG_6731).resizable().frame(width: 300, height: 300).clipShape(Circle())
+            
+        }
+    }
+}
+
+#Preview {
+    Ghadah()
+}
